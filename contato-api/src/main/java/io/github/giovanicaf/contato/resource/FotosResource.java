288cb282f0f1fe64bbb48c179182dfc14c4d4741
Giovani Campelo <giovanicaf@gmail.com>
2021-08-11 11:31:31 -0300

    first commit

---
+++ contato-api/src/main/java/io/github/giovanicaf/contato/resource/FotosResource.java
@@ -0,0 +1,20 @@
+package io.github.giovanicaf.contato.resource;
+
+import io.github.giovanicaf.contato.storage.Disco;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/fotos")
+public class FotosResource {
+	
+	@Autowired
+	private Disco disco;
+	
+	@PostMapping
+	public void upload(@RequestParam MultipartFile foto) {
+		disco.salvarFoto(foto);
+	}
+
+}
